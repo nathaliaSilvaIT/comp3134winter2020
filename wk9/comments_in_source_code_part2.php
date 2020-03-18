df7e037edaa7ee529671c8217f015cff2791c63f
nathaliaSilvaIT <44911593+nathaliaSilvaIT@users.noreply.github.com>
2020-03-17 20:58:45 -0400

    Add files via upload

---
+++ wk9/comments_in_source_code_part2.php
@@ -0,0 +1,22 @@
+<?php
+
+echo '<h1>This is an example of an H1 header tag</h1>';
+# <!--Created by Nathalia Silva with employee id: 123456-->
+
+echo '<h2>PHP</h2>';
+# <!--Changed password to 123456 -->
+
+echo '<h3>JohnComputer</h3>';
+# <!--password in his email-->
+
+echo '<h4>Another sesitive info from John Doe</h4>';
+# <!--C.C. 4040 2222 3333 4444-->
+
+echo '<h5>More sensitive info<h5>';
+# <!--Answeres of the exam-->
+
+echo '<h6>User name for computer</h6>';
+# <!--Admin is johnDoe123-->
+
+
+?>
